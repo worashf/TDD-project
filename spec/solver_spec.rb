9bc3e76aae0ba0e7df277cb222b95c105065bcd8
worashf <uppert83@gmail.com>
2022-09-02 16:53:46 +0300

    Add factorial method spec

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,9 @@ describe Solver do
     solver  = Solver.new 
     context "Factorial" do
         it "Should return the factorial of the number" do
-            
+            expect(solver.factorial(4)).to eq 24
+            expect(solver.factorial(5)).to eq 120
+            expect(solver.factorial(10)).to eq 3628800
         
         end
     end
