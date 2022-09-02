8ccba2df746ca5ed66e44e0828c65e36228fb9c7
worashf <uppert83@gmail.com>
2022-09-02 16:41:25 +0300

    Add failed test for factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,14 @@
+require './solver'
+
+
+describe Solver do
+    solver  = Solver.new 
+    context "Factorial" do
+        it "Should return the factorial of the number" do
+            
+        
+        end
+    end
+
+
+end
