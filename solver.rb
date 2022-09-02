56699abccdb87cf9c5ac3e0055eb6fd4b8e98522
worashf <uppert83@gmail.com>
2022-09-02 16:59:25 +0300

    Add factorial implementation

--- solver.rb
+++ solver.rb
@@ -1,4 +1,12 @@
 class Solver
-    
 
+    def factorial(number)
+        if number < 0 then 
+            raise ArgumentError.new 'Negative values not permitted'
+          elsif number == 0 || number == 1 then
+            return 1
+          else
+            return number * factorial(number-1) if number > 0
+          end
+    end
 end
