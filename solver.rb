7d74b8b524f50dbbd37b781bf03846fe7921b50d
worashf <uppert83@gmail.com>
2022-09-02 17:02:32 +0300

    Add spec for 0 factorial

--- solver.rb
+++ solver.rb
@@ -3,8 +3,6 @@ class Solver
     def factorial(number)
         if number < 0 then 
             raise ArgumentError.new 'Negative values not permitted'
-          elsif number == 0 || number == 1 then
-            return 1
           else
             return number * factorial(number-1) if number > 0
           end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,6 +10,9 @@ describe Solver do
             expect(solver.factorial(10)).to eq 3628800
         
         end
+        it "should return 1 if the number is 0" do
+            expect(solver.factorial(0)).to eq 1
+        end
     end
 
 
